a6c676109a4b5f84613e94d2860e45029d1a08b0
Holger Schillack <hs@jochenlinnemann.de>
2021-12-25 19:47:36 +0100

    Remove dist files

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env sh
 
+set -e
+
 npm run build
 
 cd dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'Deployment'
 git push -f git@github.com:holgerschillack/b1-to-b0-code-converter.git main:gh-pages
 
 cd -
